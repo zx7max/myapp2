46d64f8040de049abed814d45cdfe22bfb604dd8
Max Z <maximzol@yandex.ru>
2023-11-26 15:42:23 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route, Link} from "react-router-dom";
+
+import { AboutPage } from "./pages/about_page";
+import { NotFoundPage } from "./pages/404_page";
+import { HomePage } from "./pages/home_page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+      <Link to="/">хомяк</Link>
+      <Link to="/about">О странице</Link>
+
       </header>
+      <div>
+        <Routes >
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/about" element={<AboutPage/>} />
+          <Route path="*" element={<NotFoundPage/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
